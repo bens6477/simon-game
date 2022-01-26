3f4494490aee35dbc30fc4e859829c8fb0e0dbff
Ben Smith <bens6477@gmail.com>
2022-01-26 22:17:10 +0000

    Add test cases for newGame

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll( () => {
     let fs = require("fs");
@@ -27,5 +27,27 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll( () => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    })
+    test("game.score value is reset to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("game.playerMoves array is cleared", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("game.currentGame array is cleared", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
